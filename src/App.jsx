7191d1b0d196c5a49f92b77596f05ea49447ab74
Andrey Forechi <andreygamerbr2016@gmail.com>
2026-06-07 20:18:24 -0300

    Corrigido erro build 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,12 @@ import { Analytics } from "@vercel/analytics/react"
 
 export function App() {
   return (
+    
     <BrowserRouter>
+    <Analytics />
       <Routes>
 
-        <Analytics />
+        
 
         <Route
           path="/"
