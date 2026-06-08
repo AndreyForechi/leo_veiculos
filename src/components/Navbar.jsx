9102f3ada25fefaada2806fc849cc4dae2dc29f1
Andrey Forechi <andreygamerbr2016@gmail.com>
2026-06-08 09:45:11 -0300

    Ajustes nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,25 +6,25 @@ export function Navbar() {
         <header className="fixed top-0 w-full bg-black/60 backdrop-blur-xl border-b border-white/10 z-50">
             <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
                 <a onClick={() => navigate("/")}>
-                <img
-                    src="/logo_leo.jpg"
-                    alt="Logo"
-                    className="w-13 h-13 cursor-pointer rounded-full bg-white text-black text-sm flex items-center justify-center font-bold"></img>
-                <div />
-            </a>
+                    <img
+                        src="/logo_leo.jpg"
+                        alt="Logo"
+                        className="w-13 h-13 cursor-pointer rounded-full bg-white text-black text-sm flex items-center justify-center font-bold"></img>
+                    <div />
+                </a>
 
-            <nav className="flex gap-8 text-sm text-zinc-300">
-                <a href="#about" className="hover:text-white transition">
-                    Sobre
-                </a>
-                <a href="#projects" className="hover:text-white transition">
-                    Veículos
-                </a>
-                <a href="#contact" className="hover:text-white transition">
-                    Contato
-                </a>
-            </nav>
-        </div>
+                <nav className="flex gap-8 text-sm text-zinc-300">
+                    <a href="#about" className="hover:text-white transition">
+                        Sobre
+                    </a>
+                    <a onClick={() => navigate("/catalogo")} className="hover:text-white transition">
+                        Veículos
+                    </a>
+                    <a href="#contact" className="hover:text-white transition">
+                        Contato
+                    </a>
+                </nav>
+            </div>
         </header >
     )
 }
